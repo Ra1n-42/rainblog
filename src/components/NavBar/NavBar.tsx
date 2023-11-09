90d48c8f9a3850a3161734e7698b14281600a92d
Ra1n-42 <150204147+Ra1n-42@users.noreply.github.com>
2023-11-09 15:47:49 +0100

    add structure

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,38 @@
+import styles from "./NavBar.module.css";
+// import { AiOutlineClose } from "react-icons/ai";
+
+const NavLinks = [
+  { label: "Who am I?", route: "/me" },
+  { label: "How To's", route: "/archives" },
+  { label: "Journey", route: "/journey" },
+  { label: "About", route: "/about" },
+];
+const CollapsSidebar = function () {
+  var Sidebar = document.querySelector("SideBarMenuContainer");
+  Sidebar?.classList.add("collaps");
+};
+
+function NavBar() {
+  return (
+    <div className={`${styles.SideBarMenuContainer}`}>
+      <div className={styles.SideBarMenu}>
+        <div className={styles.CloseButtonContainer}>
+          <button className={styles.BtnClose} onClick={CollapsSidebar}>
+            <span>&times;</span>
+          </button>
+        </div>
+        <div className={styles.MenuLinks}>
+          <ul>
+            {NavLinks.map((item) => (
+              <li className={styles.NavBarMenuList}>
+                <a href={item.route}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/components/Sidebar/SideBar.tsx
@@ -0,0 +1,13 @@
+export default function SideBar() {
+  return (
+    <>
+      <aside className="h-screen">
+        <nav className="h-full flex flex-col bg-white border-r schadow-sm">
+          <div className="p-4 pb-2 flex justify-between items-center">
+            <ul className="flex-1 px-3"></ul>
+          </div>
+        </nav>
+      </aside>
+    </>
+  );
+}
