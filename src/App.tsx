1b8c00e122eb3655882377fb8e8545fe0d489b15
Ra1n-42 <150204147+Ra1n-42@users.noreply.github.com>
2023-11-12 17:19:17 +0100

    animated sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import { GiStairsGoal } from "react-icons/gi";
 import { SiAboutdotme } from "react-icons/si";
 import { RiSlideshow3Line } from "react-icons/ri";
 import { VscDebugStart } from "react-icons/vsc";
-const NavLinks = [
-  { label: "Start here", route: "/here", icon: <VscDebugStart /> },
-  { label: "How To's", route: "/archives", icon: <RiSlideshow3Line /> },
-  { label: "Journey", route: "/journey", icon: <GiStairsGoal /> },
-  { label: "About", route: "/about", icon: <SiAboutdotme /> },
-];
+import { useState } from "react";
 
+const NavLinks = [
+  { label: "Start here", route: "/here", icon: <VscDebugStart size={20} /> },
+  {
+    label: "How To's",
+    route: "/archives",
+    icon: <RiSlideshow3Line size={20} />,
+  },
+  { label: "Journey", route: "/journey", icon: <GiStairsGoal size={20} /> },
+  { label: "About", route: "/about", icon: <SiAboutdotme size={20} /> },
+];
+// const [theme, setTheme] = useState(true)
 function App() {
   return (
     <div className="App flex bg-slate-100">
--- src/components/Sidebar/SideBar.tsx
+++ src/components/Sidebar/SideBar.tsx
@@ -1,4 +1,5 @@
-import { BiArrowToLeft } from "react-icons/bi";
+import { useState } from "react";
+import { BiArrowToLeft, BiArrowToRight } from "react-icons/bi";
 interface NavLink {
   label: string;
   route: string;
@@ -10,21 +11,56 @@ interface Props {
 }
 
 export default function SideBar({ items }: Props) {
+  const [expanded, setExpanded] = useState(false);
   return (
     <aside className="h-screen">
-      <nav className="h-full flex flex-col bg-white border-r shadow-sm">
-        <div className="p-4 pb-2 flex justify-between items-center">
-          <img src="https://img.logoipsum.com/243.svg" alt="" />
-          <button className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100">
-            <BiArrowToLeft size={25} />
+      <nav className="h-full flex flex-col bg-white border-r shadow-sm ">
+        <div className="relative">
+          {/* <img src="https://img.logoipsum.com/243.svg" alt="" /> */}
+          <button
+            title={expanded ? "close" : "open"}
+            className={`
+            absolute p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100 top-1
+            ${expanded ? "right-2" : "left-3"}
+            `}
+            onClick={() => setExpanded((curr) => !curr)}
+          >
+            {expanded ? (
+              <BiArrowToLeft size={25} />
+            ) : (
+              <BiArrowToRight size={25} />
+            )}
           </button>
         </div>
-        <div className=" border-t flex p-3 justify-center h-full">
-          <ul className="px-3">
+        {/* Navigationskontainer */}
+        <div className="Navcontainer pt-12">
+          <ul className="NavList px-5">
             {items.map((item) => (
-              <li className="flex ">
-                {item.icon}
-                <a href={item.route}>{item.label}</a>
+              <li
+                key={item.route}
+                className="flex items-center cursor-pointer mt-10 group"
+              >
+                <a href={item.route}>{item.icon}</a>
+
+                <a
+                  className={`overflow-hidden transition-all  ${
+                    expanded ? "w-30 ml-3" : "hidden w-0"
+                  }`}
+                  href={item.route}
+                >
+                  {item.label}
+                </a>
+                {!expanded && (
+                  <div
+                    className={`
+                    absolute rounded-md px-2 py-1 left-12 bg-indigo-100 text-indigo-800 text-sm 
+                    invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100
+                    group-hover:translate-x-0 mx-5
+                    `}
+                  >
+                    {item.label}
+                  </div>
+                )}
               </li>
             ))}
           </ul>
@@ -33,12 +69,3 @@ export default function SideBar({ items }: Props) {
     </aside>
   );
 }
-
-// export default function SideBarItem({ icon, text, active, alert }) {
-//   return (
-//     <li>
-//       {icon}
-//       <span>{text}</span>
-//     </li>
-//   );
-// }
